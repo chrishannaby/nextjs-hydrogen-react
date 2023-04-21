06b976009b84dcda196405348fa9d770551b6d50
chrishannaby <chrishannaby@gmail.com>
2023-04-21 00:44:22 -0600

    starting point

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,33 +1,55 @@
 import { RadioGroup } from "@headlessui/react";
-import {
-  useProduct,
-  Image,
-  useShop,
-  useMoney,
-  ShopPayButton,
-} from "@shopify/hydrogen-react";
+import { useState } from "react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+const placeholderProduct = {
+  title: "Women's Basic Tee",
+  price: "$32",
+  rating: 3.9,
+  reviewCount: 512,
+  href: "#",
+  imageSrc:
+    "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+  imageAlt: "Back of women's Basic Tee in black.",
+  colors: [
+    { name: "Black", bgColor: "bg-gray-900", selectedColor: "ring-gray-900" },
+    {
+      name: "Heather Grey",
+      bgColor: "bg-gray-400",
+      selectedColor: "ring-gray-400",
+    },
+  ],
+  variants: [
+    { title: "XXS", availableForSale: true },
+    { title: "XS", availableForSale: true },
+    { title: "S", availableForSale: true },
+    { title: "M", availableForSale: true },
+    { title: "L", availableForSale: true },
+    { title: "XL", availableForSale: true },
+    { title: "XXL", availableForSale: false },
+  ],
+};
+
 export default function ProductCard() {
-  const shop = useShop();
-  const { product, variants, setSelectedVariant, selectedVariant } =
-    useProduct();
-  const price = useMoney(selectedVariant.price);
+  const [selectedVariant, setSelectedVariant] = useState(
+    placeholderProduct.variants[2]
+  );
 
   return (
     <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:items-center lg:gap-x-8">
-      <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
-        <Image
-          data={product.featuredImage}
+      <div className="aspect-[2/3] overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
+        <img
+          src={placeholderProduct.imageSrc}
+          alt={placeholderProduct.imageAlt}
           className="object-cover object-center"
         />
       </div>
       <div className="sm:col-span-8 lg:col-span-7">
         <h2 className="text-xl font-medium text-gray-900 sm:pr-12">
-          {product.title}
+          {placeholderProduct.title}
         </h2>
 
         <section aria-labelledby="information-heading" className="mt-1">
@@ -35,7 +57,9 @@ export default function ProductCard() {
             Product information
           </h3>
 
-          <p className="font-medium text-gray-900">{price.localizedString}</p>
+          <p className="font-medium text-gray-900">
+            {placeholderProduct.price}
+          </p>
         </section>
 
         <section aria-labelledby="options-heading" className="mt-8">
@@ -55,7 +79,7 @@ export default function ProductCard() {
                   Choose a size
                 </RadioGroup.Label>
                 <div className="grid grid-cols-7 gap-2">
-                  {variants.map((variant) => (
+                  {placeholderProduct.variants.map((variant) => (
                     <RadioGroup.Option
                       key={variant.title}
                       value={variant}
@@ -81,11 +105,12 @@ export default function ProductCard() {
                 </div>
               </RadioGroup>
             </div>
-
-            <ShopPayButton
-              variantIds={[selectedVariant?.id]}
-              storeDomain={shop.getShopifyDomain()}
-            />
+            <button
+              type="submit"
+              className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Add to bag
+            </button>
           </form>
         </section>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,62 +1,15 @@
-import { ProductProvider } from "@shopify/hydrogen-react";
 import ProductCard from "@/components/ProductCard";
-import { fetchFromStorefrontApi } from "@/lib/storefrontClient";
-
-const PRODUCT_QUERY = `
-{
-  products(first: 20) {
-    edges {
-      node {
-        id
-        title
-        description
-        featuredImage {
-          id
-          url
-        }
-        variants(first: 20) {
-          edges {
-            node {
-              id
-              title
-              availableForSale 
-              selectedOptions {
-                name
-                value
-              }
-              price {
-                amount
-                currencyCode
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`;
 
 export async function getStaticProps() {
-  const response = await fetchFromStorefrontApi(PRODUCT_QUERY);
-  const json = await response.json();
   return {
-    props: {
-      products: json?.data?.products?.edges || null,
-    },
+    props: {},
   };
 }
 
 export default function Home({ products }) {
   return (
     <main className="m-24 flex flex-col gap-12">
-      {products.map((product) => {
-        return (
-          <ProductProvider data={product.node}>
-            <ProductCard />
-          </ProductProvider>
-        );
-      })}
+      <ProductCard />
     </main>
   );
 }
