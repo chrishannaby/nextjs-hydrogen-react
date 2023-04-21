ab484ad0c336799770785536fb74d200b0261644
chrishannaby <chrishannaby@gmail.com>
2023-04-20 23:38:49 -0600

    working ShopPay button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ShopifyProvider } from "@shopify/hydrogen-react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ShopifyProvider
+      storeDomain="https://paigedemo20230411.myshopify.com"
+      storefrontToken="05e0d82260b46e7158635cd9cc3cd907"
+      storefrontApiVersion="2023-04"
+      countryIsoCode="US"
+      languageIsoCode="EN"
+    >
+      <Component {...pageProps} />
+    </ShopifyProvider>
+  );
 }
